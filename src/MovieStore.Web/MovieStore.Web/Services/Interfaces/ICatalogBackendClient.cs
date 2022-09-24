6f23824dc12eb291b43f2099b0e40cc5801082e3
Will Velida <willvelida@microsoft.com>
2022-09-24 12:34:36 +1200

    Fixing route paths in CatalogBackendClient

--- src/MovieStore.Web/MovieStore.Web/Services/Interfaces/ICatalogBackendClient.cs
+++ src/MovieStore.Web/MovieStore.Web/Services/Interfaces/ICatalogBackendClient.cs
@@ -5,19 +5,19 @@ namespace MovieStore.Web.Services.Interfaces
 {
     public interface ICatalogBackendClient
     {
-        [Get("api/Movie")]
+        [Get("/api/Movie")]
         Task<List<Movie>> GetMovies();
 
-        [Get("api/Movie/{movieId}")]
+        [Get("/api/Movie/{movieId}")]
         Task<Movie> GetMovie(int movieId);
 
-        [Put("api/Movie/{movieId}")]
+        [Put("/api/Movie/{movieId}")]
         Task UpdateMovie(int movieId, Movie movie);
 
-        [Post("api/Movie")]
+        [Post("/api/Movie")]
         Task CreateMovie(Movie movie);
 
-        [Delete("api/Movie/{movieId}")]
+        [Delete("/api/Movie/{movieId}")]
         Task DeleteMovie(int movieId);
     }
 }
