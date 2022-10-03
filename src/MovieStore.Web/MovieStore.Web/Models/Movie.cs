78afdc8173602d95c068b3ffcd4eb22b7477f8eb
Will Velida <willvelida@microsoft.com>
2022-10-03 13:43:27 +1300

    Adding more responses from API in ViewMovie

--- src/MovieStore.Web/MovieStore.Web/Models/Movie.cs
+++ src/MovieStore.Web/MovieStore.Web/Models/Movie.cs
@@ -4,11 +4,16 @@ namespace MovieStore.Web.Models
 {
     public class Movie
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; }
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        public string PosterUrl { get; set; }
         public string Genre { get; set; }
+        public string Tagline { get; set; }
+        public string Overview { get; set; }
         public decimal Price { get; set; }
+        public int Runtime { get; set; }
+        public string Classification { get; set; }
     }
 }
